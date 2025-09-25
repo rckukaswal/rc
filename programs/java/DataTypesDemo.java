c59819f5f369282648474bf477375d936808d360
rckukaswal <ramchandrakukaswal@gmail.com>
2025-09-25 19:21:23 +0530

    Create DataTypesDemo.java

---
+++ programs/java/DataTypesDemo.java
@@ -0,0 +1,13 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 20.5;
+        char c = 'A';
+        boolean d = true;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + d);
+    }
+}
